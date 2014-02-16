c6cebfa3127cda9219cedb5500ffb2d2a398fb81
Sebastian Bauer <admin@e-back.net>
2014-02-16 16:07:56 +0100

    integrate setup for python 2.x and python 3.x

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
 # -*- coding: utf-8 -*-
+import sys
 from distutils.core import setup, Extension
 
-module = Extension('aspell',
-	libraries = ['aspell'],
-	library_dirs = ['/usr/local/lib/'],
-	sources = ['aspell.c']
+if sys.version_info[0] == 2:
+    sources = ['aspell.2.c']
+else:
+    sources = ['aspell.c']
+
+module = Extension(
+    'aspell',
+    libraries=['aspell'],
+    library_dirs=['/usr/lib/'],
+    sources=sources
 )
 
-setup (name = 'aspell-python',
-	version = '1.12',
-	ext_modules = [module],
+setup(
+    name='aspell-python',
+    version='1.12',
+    ext_modules=[module],
 
-	description      = "Wrapper around GNU Aspell for python3.x",
-	author           = "Wojciech Muła",
-	author_email     = "wojciech_mula@poczta.onet.pl",
-	maintainer       = "Wojciech Muła",
-	maintainer_email = "wojciech_mula@poczta.onet.pl",
-	url              = "http://0x80.pl/proj/aspell-python",
-	classifiers      = [],
-    platforms        = "Linux, Windows",
-	license          = "BSD (3 clauses)",
+    description="Wrapper around GNU Aspell for python3.x",
+    author="Wojciech Muła",
+    author_email="wojciech_mula@poczta.onet.pl",
+    maintainer="Wojciech Muła",
+    maintainer_email="wojciech_mula@poczta.onet.pl",
+    url="http://0x80.pl/proj/aspell-python",
+    classifiers=[],
+    platforms="Linux, Windows",
+    license="BSD (3 clauses)",
 
-	long_description = """
+    long_description="""
 Aspell-python consist C extension modules both for Py2 and
 Py3k (Py3 is maintained), and also pure python module using
 ctypes to communicate with aspell library.
